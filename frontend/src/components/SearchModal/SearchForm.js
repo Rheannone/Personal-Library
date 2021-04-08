d7fe95480da3a2bea99d02d1f079804560bbf4d1
Rheannone <73176069+Rheannone@users.noreply.github.com>
2021-04-08 12:33:17 -0400

    search adding matching results to state

--- frontend/src/components/SearchModal/SearchForm.js
+++ frontend/src/components/SearchModal/SearchForm.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import * as sessionActions from "../../store/session";
 import { Redirect } from 'react-router-dom'
 import { useDispatch, useSelector } from "react-redux";
+import { searchUsers } from '../../store/friends'
 
 function SearchForm() {
     const dispatch = useDispatch();
@@ -9,16 +10,25 @@ function SearchForm() {
     const [email, setEmail] = useState("")
 
 
+    const handleSearch = e => {
+        e.preventDefault();
+        dispatch(searchUsers(email))
+
+    }
+
     return (
         <>
         <h1>Search for Friends</h1>
+        <form onSubmit={handleSearch}>
         <input
         type="text"
         placeholder="search by email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
         />
+        <button type="submit">Search</button>
 
+        </form>
         </>
     )
 };
