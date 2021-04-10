049fb396a682f53b6094e940463f5e1789c1b38a
Rheannone <73176069+Rheannone@users.noreply.github.com>
2021-04-10 14:41:31 -0400

    groovy splash is styled" "

--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -13,11 +13,11 @@ function Splash() {
 
     return (
         <>
-        <div className="auth-title">
+        <div className="splash-title">
         <h1 className='splash-groovy'>
-
+        <div className='splash-spans'>
         <span>M</span><span>y</span><span> </span><span>G</span><span>r</span><span>o</span><span>o</span><span>v</span><span>y</span><span> </span><span>L</span><span>i</span><span>b</span><span>r</span><span>a</span><span>r</span><span>y</span>
-        
+        </div>
         </h1>
         </div>
         </>
